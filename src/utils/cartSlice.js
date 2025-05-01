c7dd315e1037086eeb6086f69fe34e3bcc0a3b2d
sibuspd <sibuspd@gmail.com>
2025-05-01 21:58:06 +0530

    Reducer functions rectified

--- src/utils/cartSlice.js
+++ src/utils/cartSlice.js
@@ -8,6 +8,8 @@ const cartSlice = createSlice({
         totalQuantity: 0, // Quantity of each item to be increased or decreased
     }, 
     reducers: {
+
+        // ADDING 1 UNIT OF A PRODUCT
         addProduct: (state, action) => {
             // Before we add a product, we have to first check the Cart array i.e., items[] for existing product
             const existingItem = state.items.find((product) => product.id === action.payload.id);
@@ -18,27 +20,27 @@ const cartSlice = createSlice({
 
             state.totalQuantity += 1; // Incrementing as we added 1 unit
         },
+
+        // REMOVING 1 UNIT OF A PRODUCT
         removeProduct: (state, action) => {
             // Again checking the product existence in cart
-            const isExistingItem = state.items.find((product) => product.id === action.payload.id);
-            if(isExistingItem)
-                 isExistingItem.quantity -= 1;
-            else
-                state.items = state.items.filter( (product) => product.id !== action.payload.id); 
-
-            state.totalQuantity -= 1; // Now we decrement the number of items in cart.
+            const existingItem = state.items.find((product) => product.id === action.payload.id);
+            if(existingItem){
+                if(existingItem.quantity === 1)
+                    state.items = state.items.filter((product) => product.id !== action.payload.id); // Remove the product object from the array itself
+                else
+                existingItem.quantity -= 1; // Decrease the quantity of a product
+            }
+                 state.totalQuantity -= 1;
         },
-        modifyQuantity: (state, action) => {
-            const { id, quantity} = action.payload;
-            const isExistingItem = state.items.find( (product) => id === product.id);
-
-        },
+        
+        // STORES THE SEARCH QUERY MADE INSIDE SEARCH BAR
         setSearchWord: ( state, action) => {
             state.searchWord = action.payload; // The searchWord takes the input value from search bar
         },
     }
 });
 
-export const { addProduct, removeProduct, modifyQuantity, setSearchWord} = cartSlice.actions; // Exporting the cart actions
+export const { addProduct, removeProduct, setSearchWord} = cartSlice.actions; // Exporting the cart actions
 
 export default cartSlice.reducer;
